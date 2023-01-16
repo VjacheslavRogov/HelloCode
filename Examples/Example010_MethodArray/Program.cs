a565c17bfaffa5ad8a437e2d856f3338ce33be68
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-01-16 23:50:22 +0300

    added Example010

---
+++ Examples/Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 12, 35, 49, 53, 62, 7, 885, 12 };
+
+int n = array.Length;
+int find = 12;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
