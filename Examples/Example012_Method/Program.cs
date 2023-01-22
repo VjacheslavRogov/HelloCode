0425ffb5f71d366bd4acde4916a9b6b1e9ad6cd6
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-01-22 18:36:10 +0300

    complete Example12

--- Examples/Example012_Method/Program.cs
+++ Examples/Example012_Method/Program.cs
@@ -27,7 +27,26 @@
 
 // Вид 3
 // 
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
 
+// int year = Method3();
+// Console.WriteLine(year);
 
 // Вид 4
 // 
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "z ");
+// Console.WriteLine(res);
