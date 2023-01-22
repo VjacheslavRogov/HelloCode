84cd9e45b71cd0d005b3d9c680e2a693bbee8639
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-01-22 18:39:44 +0300

    complete Example12

--- Examples/Example012_Method/Program.cs
+++ Examples/Example012_Method/Program.cs
@@ -39,12 +39,10 @@
 // 
 // string Method4(int count, string text)
 // {
-//     int i = 0;
 //     string result = String.Empty;
-//     while (i < count)
+//     for (int i = 0; i < count; i++)
 //     {
 //         result = result + text;
-//         i++;
 //     }
 //     return result;
 // }
