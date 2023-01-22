d1690c312afddb094f3c5f405d15b389130846b0
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-01-22 18:29:35 +0300

    added Example12

---
+++ Examples/Example012_Method/Program.cs
@@ -0,0 +1,33 @@
+﻿// Вид 1
+// 
+// void Method1()
+// {
+//     Console.WriteLine("Автор ...");
+// }
+// Method1();
+
+// Вид 2
+// 
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+// Method2(msg: "Текст сообщения");
+
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine(msg);
+//         i++;
+//     }
+// }
+// Method21(msg: "Текст", count: 4);
+
+// Вид 3
+// 
+
+
+// Вид 4
+// 
